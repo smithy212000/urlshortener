1ecefe759f646428668d938bd3a62aa417c6f6f8
Hayden Smith <smithy21200@gmail.com>
2015-08-11 20:28:04 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	$domain = "http://mydomain.com"; // Set this to the domain name of the server this script is running on. Must have http://
+	$length = "5"; // Length of the short URL sequence.
+?>
