07f3c1f993d38346f52d6cbfba2e4665109d7c5a
Hayden Smith <smithy21200@gmail.com>
2015-08-13 11:17:30 +0100

    Update config.php
    Revert to old config.

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
 	$domain = "http://mydomain.com"; // Set this to the domain name of the server this script is running on. Must have http://
 	$length = "5"; // Length of the short URL sequence.
-	$redirectLength = "0"; // How long to wait before redirecting the user to the page. 0 is instant.
 ?>
