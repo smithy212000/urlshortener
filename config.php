9847c10ba4f0f5d79f69b3c73afdb28c83f61732
Hayden Smith <smithy21200@gmail.com>
2015-08-13 12:49:29 +0100

    Update config.php
    Add HTML redirect option.

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
 	$domain = "http://mydomain.com"; // Set this to the domain name of the server this script is running on. Must have http://
-	$length = "5"; // Length of the short URL sequence.
+	$length = 5; // Length of the short URL sequence. Default: 5.
+	$htmlRedirect = false; // Use HTML to redirect. Default: false - use php.
+	$redirectLength = 0; // The redirect wait time for HTML redirects, doesn't work on all browsers. Default: 0.
 ?>
